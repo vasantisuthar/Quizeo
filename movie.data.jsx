58bef0e541d0fb082bd711f81ba4aa1d3a7282f3
Vasanti Suthar <vasantisuthar15@gmail.com>
2022-01-29 01:47:09 +0530

    created movie data file

---
+++ movie.data.jsx
@@ -0,0 +1,134 @@
+const movie_data = {
+    avengers : {
+        id: 1,
+        title : "Avengers",
+        total : 5,
+        questions:[
+            {
+                id:1,
+                question:"How many rings does Captain America's shield have?",
+                answer : 2
+            },
+            {
+                id:2,
+                question:"What planet was the Soul Stone hidden on in the movie Infinity War?",
+                answer : "Vormir"
+            },
+            {
+                id:3,
+                question:"What is the real name of the Incredible Hulk?",
+                answer : "Bruce Banner"
+            },
+            {
+                id:4,
+                question:"Who took out Tony Stark's parents?",
+                answer : "Winter Soldier"
+            },
+            {
+                id:5,
+                question:"How many rings does Captain America's shield have?",
+                answer : 2
+            }
+        ]
+    },
+    moneyHeist :{
+        id: 2,
+        title : "Money Heist",
+        total : 5,
+        questions:[
+            
+                {
+                    id:1,
+                    question:" How long did the professor take to prepare for the robbery?",
+                    answer : "years"
+                },
+                {
+                    id:2,
+                    question:"How many people did the professor recruit to help him carry out his plan?",
+                    answer : "eight"
+                },
+                {
+                    id:3,
+                    question:"Inspector Raquel's mother suffered from which medical condition?",
+                    answer : "Alzheimer's disease"
+                },
+                {
+                    id:4,
+                    question:"The whole idea and plan of the heist was whose?",
+                    answer : "The professor's father"
+                },
+                {
+                    id:5,
+                    question:"How much money were the Robbers able to print?",
+                    answer : "984 million Euros"
+                }
+            
+        ]
+    },
+    friends:{
+        id:3,
+        title: "Friends",
+        total : 5,
+        questions:[
+            {
+                id:1,
+                question:"How many sisters does Joey have?",
+                answer : 7
+            },
+            {
+                id:2,
+                question:"What is the name of Joey’s character in the commercial for opening milk cartons?",
+                answer : "Kevin"
+            },
+            {
+                id:3,
+                question:"What is Ross’ profession?",
+                answer : "Palaeontologist"
+            },
+            {
+                id:4,
+                question:"Where does Ross spend the night with Emily after they first meet?",
+                answer : "A bed and breakfast in Vermont"
+            },
+            {
+                id:5,
+                question:"What song does Ross’ monkey, Marcel, keep playing?",
+                answer : "‘The Lion Sleeps Tonight‘ by The Tokens"
+            }
+        ]
+    },
+    squidGame:{
+        id:4,
+        title: "Squid game",
+        total : 5,
+        questions:[
+            {
+                id:1,
+                question:"How many contestants show up to play Squid Game?",
+                answer : 456
+            },
+            {
+                id:2,
+                question:"What shape did the old man carve in the Honeycomb challenge?",
+                answer : "star"
+            },
+            {
+                id:3,
+                question:"Who was Hwang Jun-ho looking for?",
+                answer : "His older brother"
+            },
+            {
+                id:4,
+                question:"How many games are in the competition?",
+                answer : 6
+            },
+            {
+                id:5,
+                question:"Where are the contestants held?",
+                answer : "An Island in South Korea"
+            }
+        ]
+    }
+}
+
+export default movie_data;
