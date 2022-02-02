66366c123027c644904cb9f6aef0b09f50aa54f7
Vasanti Suthar <vasantisuthar15@gmail.com>
2022-02-02 20:02:14 +0530

    tailwind css setup done

---
+++ src/components/header.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <div className="headerContainer">
+            <h1 className="text-3xl font-bold underline">Quizeo</h1>
+        </div>
+     );
+}
+ 
+export default Header;
